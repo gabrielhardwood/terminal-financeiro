1b42431b78b4f334aac81a0016011120b8331ec0
gabrielhardwood <gabrielcarvalhoassisamaral4@gmail.com>
2026-03-23 13:16:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if st.button("ANALISAR ATIVO"):
 
             # 4. Chamada da Inteligência Artificial
             genai.configure(api_key=api_key)
-            model = genai.GenerativeModel('gemini-1.5-flash')
+            model = genai.GenerativeModel('gemini-3-flash')
             
             prompt = f"""
             Aja como um analista sênior. Analise o ativo {ticker}.
